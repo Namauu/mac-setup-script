588db8b3beaa13aba3d7ed2ebd7eabf2c5e6b2f1
Jason Merrell <jason@merrellworks.com>
2024-09-12 10:00:49 -0500

    Add curl script to run mac-setup-script from Namauu GitHub repository

---
+++ curl-script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run the mac-setup-script from the Namauu GitHub repository
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Namauu/mac-setup-script/main/mac-setup-script.sh)"
